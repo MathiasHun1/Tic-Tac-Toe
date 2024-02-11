5949a5ec9f129e744eb0e00d5b9de6abf04a7d7d
MathiasHun <mathias.hun@gmail.com>
2024-02-11 18:12:29 +0100

    worgking colsole game with winner announcement

--- playground.js
+++ playground.js
@@ -1,64 +1,129 @@
-const board = 
-[
-    [5, 3, 3], 
-    [1, 2, 3],
-    [1, 2, 2]
-]
-
-function announceResult (array) {
-
-    const arr = array
-    const colsArray = convertColumnsToArr(arr)
-    const crossArray = convertCrosstoArray(arr)
-
-    //Functions
-    function isWin (arr, cols, cross) {
-        if (checkEqual(arr) || checkEqual(cols) || checkEqual(cross)) {
-            return true
-        } return false
-    }
-
-    function checkEqual (arr) {
-        const results = []
-        for (i = 0; i < arr.length; i++ ) {
-            results.push(arr[i].every(element => element === arr[i][0]))
+//Factory of a gameboard object
+function GameBoard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+        //Implementation of a gameboard in dimensions rows X columns
+    for(let i = 0; i < rows; i++) {
+        board[i] = [];
+        for(let j = 0; j < columns; j++) {
+            board[i].push(Cell())
         }
-        return results.includes(true) ? true : false
     }
 
+        //Set the given board-cell to the players token
+    const setPickedCell = (row, column, player) => {
+        board[row][column].setValue(player)
+    }
+        //Drawing the board to the console
+    const drawBoard = () => {
+        return board.map((row) => (row.map(cell => cell.getValue())))
+    }
+        //Implement the winning check
 
-    function convertColumnsToArr(arr) {
-        const columnsArr = []
-        for (i = 0; i < arr.length; i++) {
-            let column = []
-            for (j = 0; j < arr.length; j++) {
-                column.push(arr[j][i])
+    const announceResult = (array) => {
+        const arr = array
+        const colsArray = convertColumnsToArr(arr)
+        const crossArray = convertCrosstoArray(arr)
+    
+        //Functions
+        function isWin (arr, cols, cross) {
+            if (checkEqual(arr) || checkEqual(cols) || checkEqual(cross)) {
+                return true
+            } return false
+        }
+    
+        function checkEqual (arr) {
+            const results = []
+            for (i = 0; i < arr.length; i++ ) {
+                results.push(arr[i].every(element => (element === arr[i][0] && arr[i][0] !== '')))
             }
-            columnsArr.push(column)
+            return results.includes(true) ? true : false
         }
-        
-        return columnsArr
+    
+    
+        function convertColumnsToArr(arr) {
+            const columnsArr = []
+            for (i = 0; i < arr.length; i++) {
+                let column = []
+                for (j = 0; j < arr.length; j++) {
+                    column.push(arr[j][i])
+                }
+                columnsArr.push(column)
+            }
+            return columnsArr
+        }
+    
+    
+        function convertCrosstoArray(arr) {
+            const crossArray = []
+            const arr1 = []
+            const arr2 = []
+    
+            for (i = 0; i < arr.length; i++) {
+                arr1.push(arr[i][i])
+            }
+    
+            for (i = 0; i < arr.length; i++) {
+                arr2.push(arr[i][arr.length - 1- i])
+            }
+            crossArray.push(arr1, arr2)
+            return crossArray
+        }
+    
+        return isWin(arr, colsArray, crossArray)
     }
 
 
-    function convertCrosstoArray(arr) {
-        const crossArray = []
-        const arr1 = []
-        const arr2 = []
-
-        for (i = 0; i < arr.length; i++) {
-            arr1.push(arr[i][i])
-        }
-
-        for (i = 0; i < arr.length; i++) {
-            arr2.push(arr[i][arr.length - 1- i])
-        }
-        crossArray.push(arr1, arr2)
-        return crossArray
-    }
-
-    return isWin(arr, colsArray, crossArray)
+    return {board, setPickedCell, drawBoard, announceResult}
 }
 
-console.log(announceResult(board))
+//Factory for a cell object to the gameboard
+function Cell() {
+    let value = '';
+    const setValue = (player) => value = player;
+    const getValue = () => value;
+    return {setValue, getValue};
+}
+
+
+//Factory for a game-controller object
+function GameController(player1Name = 'Erzsi', player2Name = 'Dezsö') {
+    const board = GameBoard();
+    const players = [
+        {
+            name: player1Name,
+            token: 'X'
+        },
+        {
+            name: player2Name,
+            token: 'O'
+        },
+    ]
+
+    let activePlayer = players[0];
+    const getActivePlayer = () => activePlayer
+    const switchActivePlayer = () => {
+        activePlayer = getActivePlayer() === players[0] ? players[1] : players[0]
+    }
+
+    const playRound = (row, column) => {
+        if (board.board[row][column].getValue() === '') {
+
+            board.setPickedCell(row, column, getActivePlayer().token);
+            console.log(board.drawBoard())
+            if (board.announceResult(board.drawBoard())) {
+                console.log(`${getActivePlayer().name} WON THE GAME`)
+                return
+            }
+            switchActivePlayer();
+            console.log(`${getActivePlayer().name}'s turn!`);
+        } else return
+    }
+
+    return {board, playRound}
+}
+
+const game = GameController()
+console.log(game.board.drawBoard())
 
--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function GameBoard() {
     const rows = 3;
     const columns = 3;
     const board = [];
-
         //Implementation of a gameboard in dimensions rows X columns
     for(let i = 0; i < rows; i++) {
         board[i] = [];
@@ -18,12 +17,65 @@ function GameBoard() {
     }
         //Drawing the board to the console
     const drawBoard = () => {
-        console.log(board.map((row) => (row.map(cell => cell.getValue()))))
+        return board.map((row) => (row.map(cell => cell.getValue())))
     }
         //Implement the winning check
-        
 
-    return {board, setPickedCell, drawBoard}
+    const announceResult = (array) => {
+        const arr = array
+        const colsArray = convertColumnsToArr(arr)
+        const crossArray = convertCrosstoArray(arr)
+    
+        //Functions
+        function isWin (arr, cols, cross) {
+            if (checkEqual(arr) || checkEqual(cols) || checkEqual(cross)) {
+                return true
+            } return false
+        }
+    
+        function checkEqual (arr) {
+            const results = []
+            for (i = 0; i < arr.length; i++ ) {
+                results.push(arr[i].every(element => (element === arr[i][0] && arr[i][0] !== '')))
+            }
+            return results.includes(true) ? true : false
+        }
+    
+    
+        function convertColumnsToArr(arr) {
+            const columnsArr = []
+            for (i = 0; i < arr.length; i++) {
+                let column = []
+                for (j = 0; j < arr.length; j++) {
+                    column.push(arr[j][i])
+                }
+                columnsArr.push(column)
+            }
+            return columnsArr
+        }
+    
+    
+        function convertCrosstoArray(arr) {
+            const crossArray = []
+            const arr1 = []
+            const arr2 = []
+    
+            for (i = 0; i < arr.length; i++) {
+                arr1.push(arr[i][i])
+            }
+    
+            for (i = 0; i < arr.length; i++) {
+                arr2.push(arr[i][arr.length - 1- i])
+            }
+            crossArray.push(arr1, arr2)
+            return crossArray
+        }
+    
+        return isWin(arr, colsArray, crossArray)
+    }
+
+
+    return {board, setPickedCell, drawBoard, announceResult}
 }
 
 //Factory for a cell object to the gameboard
@@ -34,9 +86,13 @@ function Cell() {
     return {addValue, getValue};
 }
 
+
+
+
 //Factory for a game-controller object
 function GameController(player1Name = 'Erzsi', player2Name = 'Dezsö') {
     const board = GameBoard();
+    const boardArray = board.drawBoard()
     const players = [
         {
             name: player1Name,
@@ -56,21 +112,22 @@ function GameController(player1Name = 'Erzsi', player2Name = 'Dezsö') {
 
     const playRound = (row, column) => {
         if (board.board[row][column].getValue() === '') {
+
             board.setPickedCell(row, column, getActivePlayer().token);
-            board.drawBoard();
+            console.log(board.announceResult(boardArray))
+            console.log(board.board)
             switchActivePlayer();
             console.log(`${getActivePlayer().name}'s turn!`);
         } else return
     }
 
     const printNewRound = () => {
-        board.drawBoard();
+        console.log(boardArray);
         console.log(`${getActivePlayer().name}'s turn!`);
     }
-
     printNewRound();
 
-    return {board, playRound}
+    return {board, playRound, boardArray}
 }
 
 const game = GameController()
